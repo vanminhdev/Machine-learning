451fd49465f3e8fe91e15956a65a308203d2c42a
Lê Văn Minh <vanminh.dev@gmail.com>
2024-03-27 15:58:30 +0700

    update cnn env

--- Classification/Demo/model/cnn.py
+++ Classification/Demo/model/cnn.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
 import pandas as pd
 import numpy as np
 import tensorflow as tf
